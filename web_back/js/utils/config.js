d94ef4d31616b6032a99eb78df3f115877d3f6a8
zhangxue093 <1289912661@qq.com>
2019-09-21 19:31:36 +0800

    大事件第一天完成

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,7 @@
+
+var url={
+    user_login:'http://localhost:8000/admin/login',
+    user_logout:'http://localhost:8000/admin/logout',
+    user_xinxi:'http://localhost:8000/admin/getuser',
+    
+}
--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,7 +1,31 @@
 var user ={
+  // 登录界面
     login:function(userName,password,callback){
-        $.post('http://localhost:8000/admin/login',{user_name:userName,password:password},function(res){
+        $.post(url.user_login,{user_name:userName,password:password},function(res){
           callback(res);
         })
+    },
+  //  退出功能
+     logout:function(callback){
+      $.ajax({
+        url:url.user_logout,
+        type:'post',
+        success:function(res){
+        callback(res);
+      }
+      })
+    },
+    
+    // 获取管理员信息
+    getInfo:function(callback){
+      $.ajax({
+        url: url.user_xinxi,
+        type:'get',
+        success:function(res){
+          callback(res) ;
+          }
+        
+      })
     }
-}
+
+  }
